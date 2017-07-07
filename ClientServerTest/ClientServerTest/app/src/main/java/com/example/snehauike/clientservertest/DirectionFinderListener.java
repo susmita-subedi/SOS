e84bc43ba3d15a9b31110a592f347d9a0d6edd03
susmita-subedi <sushmita.subedi@gmail.com>
2017-07-07 07:42:44 -0700

    remove old clientserver

--- ClientServerTest/ClientServerTest/app/src/main/java/com/example/snehauike/clientservertest/DirectionFinderListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.snehauike.clientservertest;
-
-import java.util.List;
-
-public interface DirectionFinderListener {
-    void onDirectionFinderStart();
-    void onDirectionFinderSuccess(List<Route> route);
-}
